0d4212845065911ce1cdaad5f1c1372de15616ed
Kate <xuse.exe@gmail.com>
2019-04-15 17:32:54 +0300

    Lesson #3 12.04.2019 Homework

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,7 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,6 +13,12 @@ public class LoginTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.linkedin.com");
         Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up ");
+        WebElement login = driver.findElement(By.xpath("//input[@name='session_key']"));
+        login.sendKeys("bugz.and.drugz@gmail.com");
+        WebElement password = driver.findElement(By.xpath("//input[@name='session_password']"));
+        password.sendKeys("149600");
+        driver.findElement(By.xpath("//input[@id='login-submit']")).click();
+        Assert.assertEquals(driver.getTitle(), "LinkedIn");
         driver.quit();
     }
 }
